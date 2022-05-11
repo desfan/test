7090e4848b33d1346ca17538d3c038daff09f954
desfan <37998679+desfan@users.noreply.github.com>
2022-05-11 17:48:08 +0300

    test commit

---
+++ script.js
@@ -0,0 +1,28 @@
+
+// const result = alert("Hello");
+// const result = confirm("Are you here?");
+// const result = +prompt("Вам уже есть 18?", "18");
+
+// console.log(result);
+
+// const answers = [];
+
+// answers[0] = prompt('Как Вас зовут?', '');
+// answers[1] = prompt('Как Ваша фамиля?', '');
+// answers[2] = prompt('Сколько Вам лет?', '');
+
+// document.write(answers);
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Сергей'
+
+// alert(`Привет, ${user}`);
+
+console.log('Привет, ' + 'мир!');
+console.log('array' + ' - object');
+console.log('10' + 5);
+console.log(5 + '10');
+
